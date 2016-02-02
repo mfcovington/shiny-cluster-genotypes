9748ca1e5f136cd575709c9adbd40d8379dd04ac
Mike Covington <mfcovington@gmail.com>
2016-02-02 12:08:03 +0900

    Plot dendrogram

--- bin/cluster.R
+++ bin/cluster.R
@@ -1,6 +1,7 @@
 library(cluster)
 library(plyr)
 
+experiment.id <- 'SNP Data'
 input.file <- 'sample-data.snps.txt'
 
 na.strings <- c('N', 'U')
@@ -12,3 +13,8 @@ genotypes <- as.data.frame(t(genotypes))
 genotypes <- colwise(as.factor)(genotypes)
 
 distances <- daisy(genotypes, metric = 'gower')    # gower for non-numeric data
+tree <- hclust(distances, method = 'complete')
+
+plot.title <- paste('Samples Clustered by Markers', experiment.id, sep = '\n')
+plot(tree, labels = sample.ids, main = plot.title, sub = '',
+     xlab = 'Distances (complete method)')
