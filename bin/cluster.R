98b5030858dc9bb7fd7343c9798e1406fd63f07f
Mike Covington <mfcovington@gmail.com>
2016-02-02 12:07:16 +0900

    Cluster samples based on marker genotypes

--- bin/cluster.R
+++ bin/cluster.R
@@ -1,6 +1,14 @@
+library(cluster)
+library(plyr)
+
 input.file <- 'sample-data.snps.txt'
 
 na.strings <- c('N', 'U')
 genotypes <- read.table(file = input.file, sep = '\t', header = TRUE,
                         row.names = 1, na.strings = na.strings)
 sample.ids <- colnames(genotypes)
+
+genotypes <- as.data.frame(t(genotypes))
+genotypes <- colwise(as.factor)(genotypes)
+
+distances <- daisy(genotypes, metric = 'gower')    # gower for non-numeric data
