a516f9a04f3d1be8fb7b2e537726d5f819f6ea99
Mike Covington <mfcovington@gmail.com>
2016-02-04 19:11:54 +0900

    Plot samples clustered by genotype

--- bin/cluster.R
+++ bin/cluster.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(ggrepel)
 library(limma)
 library(plyr)
+library(reshape)
 
 experiment.id <- 'SNP Data'
 input.file <- 'sample-data.snps.txt'
@@ -24,12 +25,32 @@ ggplot(mds.df) +
   xlab('Dimension 1') +
   ylab('Dimension 2')
 
-genotypes <- as.data.frame(t(genotypes))
-genotypes <- colwise(as.factor)(genotypes)
+genotypes.transposed <- as.data.frame(t(genotypes))
+genotypes.transposed <- colwise(as.factor)(genotypes.transposed)
 
-distances <- daisy(genotypes, metric = 'gower')    # gower for non-numeric data
+# gower for non-numeric data
+distances <- daisy(genotypes.transposed, metric = 'gower')
 tree <- hclust(distances, method = 'complete')
 
 plot.title <- paste('Samples Clustered by Markers', experiment.id, sep = '\n')
 plot(tree, labels = sample.ids, main = plot.title, sub = '',
      xlab = 'Distances (complete method)')
+
+clustered.sample.order <- tree$order
+genotypes.sorted <- genotypes[, clustered.sample.order]
+genotypes.sorted$marker.id <- rownames(genotypes.sorted)
+
+bin.genotypes.m <- melt(genotypes.sorted, id = c('marker.id'),
+                        variable_name = 'sample')
+bin.genotypes.m$value <- as.factor(bin.genotypes.m$value)
+
+allele.colors <- c('skyblue', 'orange', 'black', 'green', 'yellow', 'plum')
+
+ggplot(bin.genotypes.m) +
+  geom_raster(aes(x = marker.id, y = sample, fill = value)) +
+  ggtitle('Samples Clustered By Genotype') +
+  xlab('Markers') +
+  ylab('Samples') +
+  labs(fill = 'Genotype') +
+  scale_fill_manual(values = allele.colors) +
+  theme(axis.text.x = element_text(angle = 90))
